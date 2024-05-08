d6ff3d6aabe807501933ae1599f08743ae0e5f3e
Ramana Venkata <vramana@users.noreply.github.com>
2024-05-08 23:25:42 +0530

    Fix

--- services/handleCsv.ts
+++ services/handleCsv.ts
@@ -1,9 +1,7 @@
-
-import fs from "fs/promises"
+import fs from "fs/promises";
 
 async function hello() {
-    const data = await fs.readFile("./public/sbi.csv", 'utf-8');
-    console.log(data)
+  const data = await fs.readFile("./public/sbi.csv", "utf-8");
+  console.log(data);
 }
 
-hello()
