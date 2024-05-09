916879839c08ad20dc74866654081129ece41e16
Ramana Venkata <vramana@users.noreply.github.com>
2024-05-09 14:51:18 +0530

    Add prisma schema

--- services/handleCsv.js
+++ services/handleCsv.js
@@ -65,4 +65,76 @@ async function readSbiStatement() {
   console.log(transactions);
 }
 
-readSbiStatement();
+// readSbiStatement();
+//
+async function readCoralCreditCard() {
+  let data = await fs.readFile("./public/coralCard.csv", "utf-8");
+  data = data.replaceAll("\r", "");
+  data = data
+    .split("\n")
+    .filter((line) => (line.match(/,/g) || []).length > 3)
+    .join("\n");
+
+  const records = await new Promise((resolve) => {
+    csv.parse(data, {}, (err, output) => {
+      resolve(output);
+    });
+  });
+
+  const headers = records[0];
+
+  const transactions = records
+    .slice(1)
+    .map((record) => {
+      const transaction = {};
+      record.forEach((value, index) => {
+        value = headers[index].startsWith("Amount") ? Number(value) : value;
+
+        transaction[headers[index]] = value;
+      });
+      return transaction;
+    })
+    .filter(
+      (transaction) =>
+        !transaction["Transaction Details"].includes("INFINITY PAYMENT")
+    );
+
+  console.log(transactions);
+}
+
+// readCoralCreditCard();
+
+async function readIciciStatement() {
+  let data = await fs.readFile("./public/icici_bank.csv", "utf-8");
+  data = data.replaceAll("\r", "");
+  data = data
+    .split("\n")
+    .filter((line) => line.split(",").filter((x) => x.length > 0).length > 5)
+    .join("\n");
+
+  const records = await new Promise((resolve) => {
+    csv.parse(data, {}, (err, output) => {
+      resolve(output);
+    });
+  });
+
+  const headers = records[0];
+
+  const transactions = records.slice(1).map((record) => {
+    const transaction = {};
+    record.forEach((value, index) => {
+      if (headers[index].trim() === "") {
+        return;
+      }
+
+      value = headers[index].startsWith("Amount") ? Number(value) : value;
+
+      transaction[headers[index]] = value;
+    });
+    return transaction;
+  });
+
+  console.log(transactions);
+}
+
+readIciciStatement();
