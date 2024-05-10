0327d06259bb13b406d21dcfe07784a50a466c0e
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-05-10 18:36:12 +0530

    render transactions in table

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { prisma } from "../services/prisma.server";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +9,56 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const transactions = await prisma.Transaction.findMany();
+  console.log("transactions", transactions);
+  return transactions;
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  console.log(data);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <div
+        id="jobs"
+        className="relative overflow-x-auto shadow-md sm:rounded-lg lg:px-16 xl:px-32 py-16"
+      >
+        <table className="w-full text-base text-left   ">
+          <thead className="text-base shadow-sm text-gray-700 uppercase">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Source
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Amount
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Details
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tag
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((t) => {
+              return (
+                <tr key={t.id} className="border-y-4 border-white shadow-sm">
+                  <td className="px-6 py-4">{t.date}</td>
+                  <td className="px-6 py-4 ">{t.sourceId}</td>
+                  <td className="px-6 py-4">{t.amount}</td>
+                  <td className="px-6 py-4">{t.details}</td>
+                  <td className="px-6 py-4"></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
