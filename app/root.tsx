9e91c27c8172baf02ed421c3fdbe9eefc42e15df
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-05-14 15:57:03 +0530

    add navigation

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,52 @@ import { Toaster } from "react-hot-toast";
 
 import "./tailwind.css";
 
+function Nav() {
+  return (
+    <nav className="bg-gray-600 border-gray-200 ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="public/icon.png" className="h-8" alt="Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+            Finance Manager
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-dropdown"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="navbar-dropdown"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+          <button className="self-center text-lg   text-white">
+            <a href="/upload-csv" aria-current="page">
+              Upload Transactions
+            </a>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -19,7 +65,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <Nav />
+        <div className="container mx-auto px-4">{children}</div>
         <ScrollRestoration />
         <Scripts />
         <Toaster />
