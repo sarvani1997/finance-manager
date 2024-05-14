3eae4cb91794a0dc0ab6e612c1b818fd95472579
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-05-14 18:34:36 +0530

    manage ignored transactions

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "@remix-run/react";
 import { DateTime } from "luxon";
 
 import { prisma } from "../services/prisma.server";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Finance Manager" }];
@@ -19,6 +20,9 @@ export const loader = async () => {
 
 export default function Index() {
   const [transactions, sources, tags] = useLoaderData<typeof loader>();
+  const [ignore, setIgnore] = useState(true);
+  const ignoredTransactions = transactions.filter((t) => !t.ignore);
+  const filteredTransactions = ignore ? ignoredTransactions : transactions;
 
   if (transactions.length === 0) {
     return (
@@ -33,10 +37,23 @@ export default function Index() {
       </div>
     );
   }
-
   return (
     <div className="py-4">
-      <div className="relative overflow-x-auto">
+      <div>
+        <label className="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            value={ignore}
+            onChange={(e) => setIgnore(!e.target.checked)}
+            className="sr-only peer"
+          />
+          <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ms-3 text-sm font-medium text-gray-900 ">
+            Show Ignored Transactions
+          </span>
+        </label>
+      </div>
+      <div className="relative overflow-x-auto mt-6">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500   ">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
@@ -61,7 +78,7 @@ export default function Index() {
             </tr>
           </thead>
           <tbody>
-            {transactions.map((t) => {
+            {filteredTransactions.map((t) => {
               return (
                 <tr key={t.id} className="bg-white border-b ">
                   <td className="px-6 py-4">
@@ -73,8 +90,17 @@ export default function Index() {
                     {sources.find((s) => t.sourceId === s.id)?.name}
                   </td>
                   <td className="px-6 py-4">Rs. {t.amount}/-</td>
-                  <td className="px-6 py-4">{t.details}</td>
-                  <td className="px-6 py-4"></td>
+                  <td className="px-6 py-4">
+                    {t.details}
+                    {t.ignore && (
+                      <span className="m-2 bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+                        Ignored
+                      </span>
+                    )}
+                  </td>
+                  <td className="px-6 py-4">
+                    {tags.find((s) => t.tagId === s.id)?.name}
+                  </td>
                   <td className="px-6 py-4">
                     <a href={`/edit-transaction/${t.id}`}>Edit</a>
                   </td>
--- app/routes/edit-transaction.$id.tsx
+++ app/routes/edit-transaction.$id.tsx
@@ -112,7 +112,7 @@ export default function EditTransaction() {
           <select
             id="tag"
             name="tag"
-            value={tag}
+            defaultValue={transaction.tagId || ""}
             onChange={(e) => setTag(e.target.value)}
             className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
           >
