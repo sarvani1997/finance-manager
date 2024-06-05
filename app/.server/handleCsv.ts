fab747a9716942b3e02341365e1f8bcb8a6ede75
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-06-05 13:19:01 +0530

    add hdfc handler

--- app/.server/handleCsv.ts
+++ app/.server/handleCsv.ts
@@ -75,6 +75,70 @@ console.log({count})
   }
 }
 
+export async function readHdfcStatement(data: string) {
+  try {
+    data = data.replaceAll("\r", "");
+    data = data
+    .split("\n")
+    .filter((line) =>  {
+      let len = line.split(",").filter((x) => x.length > 0).length
+      return len === 6 || len === 7
+    }).slice(0,-1)
+
+    data = Object.values(data).filter((x) => !x.includes("*")).join("\n");
+    
+    const records = (await new Promise((resolve) => {
+      csv.parse(data, {}, (err, output) => {
+        resolve(output);
+      });
+    })) as string[][];
+
+    const headers = records[0];
+
+    let transactions = records.slice(1).map((record) => {
+      const transaction: Transaction = {};
+      record.forEach((value, index) => {
+        transaction[headers[index]] = value;
+      });
+      return transaction;
+    });
+
+    let inserts = transactions.map((t) => {
+      const formattedDate = DateTime.fromFormat(t["Date"], "dd/MM/yy");
+
+      const amount =
+        t["Withdrawal Amt."] === " "
+          ? -Number(t["Deposit Amt."].trim().replaceAll(",", ""))
+          : Number(t["Withdrawal Amt."].trim().replaceAll(",", ""));
+      return {
+        date: formattedDate.toISO(),
+        amount: amount,
+        details: t["Narration"],
+        sourceId: 5,
+      };
+    });
+
+    let count = 0
+    console.log({inserts})
+
+    for (let transaction of inserts) {
+      try {
+        await prisma.transaction.create({
+          data: transaction
+        });
+        count = count + 1;
+      } catch (err) {
+        console.log({err})
+      }
+    }
+    console.log({count})
+    return {count};
+  } catch (err) {
+    return "error";
+  }
+}
+
+
 export async function readSbiStatement(data: string) {
   try {
     data = data.replaceAll("\r", "");
@@ -264,6 +328,7 @@ export const statementMap = {
   sbi: readSbiStatement,
   coral_cc: readCoralCreditCard,
   icici_643: readIciciStatement,
+  hdfc:readHdfcStatement
 };
 
 const validataSchema = z.object({
@@ -272,6 +337,8 @@ const validataSchema = z.object({
     z.literal("sbi"),
     z.literal("coral_cc"),
     z.literal("icici_643"),
+    z.literal("hdfc"),
+
   ]),
   data: z.string(),
 });
